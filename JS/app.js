5f388648640b205d2db92ae746d58040b7552203
Nuru <125151999+NuruHuseynov@users.noreply.github.com>
2024-04-01 10:30:15 +0400

    Update app.js

--- JS/app.js
+++ JS/app.js
@@ -1,44 +1,61 @@
-let input =document.querySelector('#toDoInput')
-let button = document.querySelector('#addButton')
-let list = document.querySelector('#toDoList')
-let arr = []
+const input = document.querySelector('#toDoInput');
+const button = document.querySelector('#addButton');
+const list = document.querySelector('#toDoList');
+let arr = [];
 
+button.addEventListener('click', addItem);
+input.addEventListener('keypress', function(event) {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        addItem();
+    }
+});
 
+function addItem() {
+    const infoValue = input.value.trim();
+    if (infoValue !== '') {
+        arr.push(infoValue);
+        renderList();
+    } else {
+        alert('Write something');
+    }
+}
 
 function deleteItem(index) {
     arr.splice(index, 1);
     renderList();
 }
 
-function renderList() {
-    list.innerHTML = '';
-    for (let i = 0; i < arr.length; i++) {
-        list.innerHTML += `<li class="lii">${arr[i]}<button class="delete" onclick="deleteItem(${i})">Delete</button></li>`;
-    }
+function editItem(index, newValue) {
+    arr[index] = newValue;
+    renderList();
 }
 
-button.addEventListener('click', function() {
-    let infoValue = input.value;
-    if (infoValue !== '') {
-        let a = input.value;
-        arr.push(a);
-        for (let  i= 0;  i< arr.length; i++) {
-            list.innerHTML += `<li class ="lii" >${arr[i]}<button class = "delete">Delete</button></li>`
-        }
-    } else {
-        alert('Write something');
-    }
-});
+function renderList() {
+    list.innerHTML = '';
+    arr.forEach((item, index) => {
+        const listItem = document.createElement('li');
+        listItem.classList.add('lii');
+        listItem.textContent = item;
 
-input.addEventListener('keypress', function(event) {
-    if (event.key === "Enter") {
-        event.preventDefault();
-        let a = input.value;
-        arr.push(a);
-        for (let  i= 0;  i< arr.length; i++) {
-            list.innerHTML += `<li class ="lii" >${arr[i]}<button class = "delete">Delete</button></li>`
-        }
-    }
-});
+        const deleteButton = document.createElement('button');
+        deleteButton.classList.add('delete');
+        deleteButton.textContent = 'Delete';
+        deleteButton.addEventListener('click', () => deleteItem(index));
 
+        const editButton = document.createElement('button');
+        editButton.classList.add('edit');
+        editButton.textContent = 'Edit';
+        editButton.addEventListener('click', () => {
+            const newValue = prompt('Edit your to-do:', item);
+            if (newValue !== null) {
+                editItem(index, newValue.trim());
+            }
+        });
 
+        listItem.appendChild(editButton);
+        listItem.appendChild(deleteButton);
+
+        list.appendChild(listItem);
+    });
+}
